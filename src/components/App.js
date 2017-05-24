7255cca5ab15536139a419dc19a9eabcebf5552e
chaice <chaice@brandwatch.com>
2017-05-24 23:26:25 +0100

    Get bootstrap working

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import NavbarInstance from './Nav'
 
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MySecondComponent = React.createClass({
+const App = React.createClass({
   render () {
-    return (h1(null, this.props.myArgument))
-  }
-})
-
-var MyFirstComponent = React.createClass({
-  render: function () {
     return (
-      div(null,
-        React.createElement(MySecondComponent, {myArgument: 'Hello there'})
-      )
+      <div id='app'>
+        <NavbarInstance />
+      </div>
     )
   }
 })
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
